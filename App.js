439bca4c87c9c6fcd1c3509368b3f080ed35ac29
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-04-17 16:00:55 -0500

    Nested navigation

--- App.js
+++ App.js
@@ -1,96 +1,252 @@
 import "react-native-gesture-handler";
 import React, { Component, useState } from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View, TextInput, Button, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, TouchableHighlight, FlatList } from 'react-native';
 
-const MainStack = createStackNavigator();
-const RootStack = createStackNavigator();
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
-function HomeScreen({ navigation }) {
-  const [text, setText] = useState("");
+function YourProfile() {
+  const DATA = [
+    {
+      id: '1',
+      title: "First Item"
+    },
+    {
+      id: '2',
+      title: "Second Item"
+    },
+    {
+      id: '3',
+      title: "Third Item"
+    },
+    {
+      id: '4',
+      title: "Fourth Item"
+    },
+    {
+      id: '5',
+      title: "Fifth Item"
+    },
+    {
+      id: '6',
+      title: "Sixth Item"
+    },
+    {
+      id: '7',
+      title: "Seventh Item"
+    },
+    {
+      id: '8',
+      title: "Eigth Item"
+    },
+    {
+      id: '9',
+      title: "Ninth Item"
+    },
+    {
+      id: '10',
+      title: "Tenth Item"
+    },
+    {
+      id: '11',
+      title: "Eleventh Item"
+    },
+    {
+      id: '12',
+      title: "Twelveth Item"
+    },
+  ];
+
+  function Item({title}) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.containertwo}>
-      <Text style={styles.title}>Punchcard</Text>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Hello, type in here"
-        onChangeText={text => setText(text)}
-        defaultValue={text}
-      />
-      <TextInput style={styles.textInput} />
       <View style={styles.container}>
-        <TouchableHighlight style={styles.button}>
-          <Button
-            onPress={() => {
-              alert("You tapped the button!");
-            }}
-            title="Register"
-            type="outline"
-          />
-        </TouchableHighlight>
-        <TouchableHighlight style={styles.button}>
-            <Button
-              title="Log In"
-              onPress={() => navigation.navigate('Root')}
-            />
-        </TouchableHighlight>
+        <Text style={styles.title}>Pri</Text>
       </View>
+      <FlatList
+        style={{flex:2}}
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title}/>}
+      >
+      </FlatList>
     </View>
   );
 }
 
-function ModalScreen({ navigation }) {
+function YourCity({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Register</Text>
-      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    <View>
+      <Text style={styles.title}>Your City Goes Here</Text>
     </View>
   );
 }
+
+function CityStack({ navigation }) {
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#243447"
+          },
+          headerLeft: () => (
+            <Button title="<" onPress={() => navigation.navigate("Home")} />
+          ),
+          headerTintColor: "#fff"
+        }}
+      >
+        <Stack.Screen name="Your City" component={YourCity} />
+      </Stack.Navigator>
+      <Button title="Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+}
+
+function CityScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName="YourCity"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#243447"
+        },
+        headerTintColor: "#fff"
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeContent} />
+      <Tab.Screen name="Notifications" component={Notifications} />
+    </Tab.Navigator>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeStack}
+      />
+      <Tab.Screen name="Notifications" component={Notifications} />
+    </Tab.Navigator>
+  )
+}
+
+function Notifications() {
+  return (
+    <View>
+      <Text style={styles.title}>hi</Text>
+    </View>
+  );
+}
+
+function HomeContent() {
+  return (
+    <View style={styles.containertwo}>
+      <Text style={styles.title}>Hello</Text>
+    </View>
+  );
+}
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#243447"
+        },
+        headerTintColor: '#fff'
+      }}
+    >
+      <Stack.Screen
+        name="Feed"
+        component={HomeContent}
+      />
+    </Stack.Navigator>
+  )
+}
+
+function HomeScreen() {
+  return (
+  <Tab.Navigator>
+      <Tab.Screen 
+        name="Home"
+        component={HomeStack}
+      />
+      <Tab.Screen name="Notifications" component={Notifications} />
+  </Tab.Navigator>
+  )
+}
+
+const MyTheme = {
+  dark: true,
+  colors: {
+    primary: "white",
+    background: "#101820",
+    card: "#243447",
+    text: "white",
+    border: "gray"
+  }
+};
 
 export default function App() {
+  const ref = React.useRef(null);
+
   return (
-    <NavigationContainer>
-      <MainStack.Navigator initialRouteName="Home" mode="modal">
-        <MainStack.Screen
+      <NavigationContainer ref={ref} theme={MyTheme}>
+      <Drawer.Navigator
+        drawerStyle={{
+          backgroundColor: '#243447'
+        }}
+      >
+        <Drawer.Screen
           name="Home"
           component={HomeScreen}
-          options={{ headerShown: false }}
+          />
+        <Drawer.Screen
+          name="Profile"
+          component={YourProfile}
         />
-        <MainStack.Screen 
-          name="Root" 
-          component={Root}
-          options={{ headerShown: false }}
-           />
-      </MainStack.Navigator>
-    </NavigationContainer>
+        <Drawer.Screen
+          name="YourCity"
+          component={CityStack}
+        />
+      </Drawer.Navigator>
+      </NavigationContainer>
   );
-}
-
-function Root() {
-  return (
-      <RootStack.Navigator>
-        <RootStack.Screen 
-          name="Register" 
-          options={{ headerShown: false }}
-          component={ModalScreen} />
-      </RootStack.Navigator>
-  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 200,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 0.5,
+    backgroundColor: "#243447",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: "gray",
+    borderBottomWidth: StyleSheet.hairlineWidth
   },
   containertwo: {
-    flex: 2,
-    backgroundColor: "#cbd097",
-    padding: 125,
-    alignItems: "center",
-    justifyContent: "space-between"
+    flex: 1
+    // flexDirection: "column"
+  },
+  item: {
+    backgroundColor: "#243447",
+    padding: 20,
+    borderBottomColor: "gray",
+    borderBottomWidth: StyleSheet.hairlineWidth
+    // marginHorizontal: 16
   },
   textInput: {
     backgroundColor: "#fff",
@@ -101,8 +257,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 46,
-    color: "#fff",
-    width: 215
+    color: "#fff"
   },
   button: {
     backgroundColor: "#fff",
