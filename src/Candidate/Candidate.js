b97a02312dfb03a53af85d78f447cc7d44d53b3b
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-05-16 18:50:50 -0500

    Create candidate data

--- src/Candidate/Candidate.js
+++ src/Candidate/Candidate.js
@@ -5,9 +5,26 @@ import {
   View,
   TouchableHighlight,
   Image,
+  FlatList,
+  ScrollView
 } from "react-native";
 
 export function Candidate({navigation}) {
+  const Data = [
+    "Healthcare - Public over private",
+    "Economy - End Wall St. subsidies, rejuvenate middle class",
+    "Energy - Renewable Energy",
+    "Climate - Change is Real",
+    "International Relations - Support democracies while working with diverse governments"
+  ]
+
+  const Data2 = [
+    "Bill 122 - Yes",
+    "Bill 303 - No",
+    "Bill 101 - Yes",
+    "Bill 501 - No"
+  ]
+
   return (
     <View style={{ flex: 1 }}>
       <Image resizeMode="cover" style={styles.image} source={require("../../assets/profilepic1a.jpg")} />
