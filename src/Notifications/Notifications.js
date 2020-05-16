98613390cbb0b57ff124c8ba4035e513bf269997
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-05-16 17:17:23 -0500

    Add dummy notifications

--- src/Notifications/Notifications.js
+++ src/Notifications/Notifications.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 function Notifications() {
+  function Item({text, id}) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{text}</Text>
+      </View>
+    )
+  }
+
+  const Data = [{
+    id: "0",
+    text: "Your state congress representative is about the vote on Bill 101! See the livestream/ place your vote in the survey/ follow this legislator's voting record."
+  },
+  {
+    id: "1",
+    text: "Your senator is scheduled to vote for a bill you bookmarked. Send them an email to make your voice heard!"
+  },
+  {
+    id: "2",
+    text: "We noticed your house representative is voting against your preferences a lot. Here is a challenger you can support in the upcoming election on April 22nd!"
+  }
+]
+
   return (
-    <View>
-      <Text style={styles.title}>Notifications</Text>
+    <View style={{flex: 1}}>
+      <FlatList
+        data={Data}
+        renderItem={({item}) => (
+          <Item
+            text={item.text}
+            id={item.id}
+            key={item.id}
+          />
+        )}
+      />
     </View>
   );
 }
@@ -21,19 +52,20 @@ export function NotificationsStack() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
     flex: 0.5,
-    backgroundColor: "#243447",
     flexDirection: "row",
     justifyContent: "space-between",
     borderBottomColor: "gray",
     borderBottomWidth: StyleSheet.hairlineWidth,
+    padding: 15,
   },
   containertwo: {
     flex: 1,
   },
   title: {
-    fontSize: 46,
+    flex: 1,
+    fontSize: 14,
     color: "#fff",
   },
 });
