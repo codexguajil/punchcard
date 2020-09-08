de4302e8f76bee0ce224065bae379922f422d78a
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-09-08 16:16:32 -0500

    Create firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDEIIQqKxiv6sekvU2Gsp54gNdOuTtL1Aw',
+  authDomain: 'punchcard-df2bf.firebaseapp.com',
+  databaseURL: 'https://punchcard-df2bf.firebaseio.com',
+  projectId: 'punchcard-df2bf',
+  storageBucket: 'punchcard-df2bf.appspot.com',
+  messagingSenderId: '845658954460',
+  appId: '1:845658954460:ios:76306bdee00736f42a3219'
+};
