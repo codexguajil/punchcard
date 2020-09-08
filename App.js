4507668717ac4e82ae887c2134e7ea1a5dccfdd7
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-09-08 17:07:55 -0500

    Move app file to root directory

---
+++ App.js
@@ -0,0 +1,50 @@
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen, MainScreen, RegistrationScreen } from './src/Screens';
+import { decode, encode } from 'base-64'
+if (!global.btoa) { global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
+const Stack = createStackNavigator();
+
+const MyTheme = {
+  dark: true,
+  colors: {
+    primary: "white",
+    background: "#1B2737",
+    card: "#243447",
+    text: "white",
+    border: "gray",
+  },
+};
+  
+export default function App() {
+  
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator>
+        { user ? (
+          <Stack.Screen name="Main">
+            {props => <MainScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+            />
+            <Stack.Screen
+              name="Registration"
+              component={RegistrationScreen}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
