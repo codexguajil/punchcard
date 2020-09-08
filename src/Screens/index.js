fbaaae2b037371206b56cb65891ba40af66121ba
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-09-08 17:10:33 -0500

    Export screens from root screen directory

---
+++ src/Screens/index.js
@@ -0,0 +1,5 @@
+export { default as LoginScreen } from './Login/Login'
+
+export { default as MainScreen } from './Main/Main'
+
+export { default as RegistrationScreen } from './Registration/Registration'
