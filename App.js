c199912127bcb14161f0442f19dda14adfb0a937
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-09-10 16:23:54 -0500

    persist login

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { LoginScreen, MainScreen, RegistrationScreen } from './src/Screens';
+import { Text, View } from 'react-native';
 import { decode, encode } from 'base-64'
 if (!global.btoa) { global.btoa = encode }
 if (!global.atob) { global.atob = decode }
+import { firebase } from './src/firebase/config';
 
 const Stack = createStackNavigator();
 
@@ -24,6 +26,33 @@ export default function App() {
   
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
+
+  // if (loading) {
+  //   return (
+  //   <></>
+  //   )
+  // }
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection("users");
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data();
+            setLoading(false);
+            setUser(userData);
+          })
+          .catch((error) => {
+            setLoading(false);
+          });
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
 
   return (
     <NavigationContainer theme={MyTheme}>
