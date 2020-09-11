2e46c48911096512f000782a59cef97127c6cead
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-09-11 16:23:49 -0500

    Use firebase and context hook to log in user

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,17 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
+import { firebase } from '../../firebase/config';
+import AuthContext from "../../../AuthContext";
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {setUser} = useContext(AuthContext)
 
   const onFooterLinkPress = () => {
     navigation.navigate('Registration')
   }
 
   const onLoginPress = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        const uid = response.user.uid
+        const usersRef = firebase.firestore().collection('users')
+        usersRef
+          .doc(uid)
+          .get()
+          .then(firestoreDocument => {
+            if(!firestoreDocument.exists) {
+              alert("User does not exist anymore.")
+              return;
+            }
+            const user = firestoreDocument.data()
+            setUser(user)
+            navigation.navigate("Main", {
+              screen: "Home",
+              params: { user },
+            });
+          })
+          .catch(error => {
+            alert(error)
+          });
+      })
+      .catch(error => {
+        alert(error)
+      })
   }
 
   return (
